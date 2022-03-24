e6539c6872d2ab688f9ee24d4decc9dfc241e1c0
tomasDelizia <tomidelizia@gmail.com>
2022-03-24 18:11:18 -0300

    1 file modified

--- TestChiCuadrado.ts
+++ TestChiCuadrado.ts
@@ -5,20 +5,46 @@ export class TestChiCuadrado {
     19.675, 21.026, 22.362, 23.685, 24.996, 26.296, 27.587, 28.869, 30.144, 31.41,
     32.671, 33.924, 35.172, 36.415, 37.652, 38.885, 40.113, 41.337, 42.557, 43.773,
   ];
+  private v: number;
+  private tabla: string[][];
+  private estadisticoAcum: number
 
   public pruebaChi(cantIntervalos: number, rnds: number[]): number {
     let limInferior: number = 0;
     const anchoIntervalo: number = 1 / cantIntervalos;
     const frecEsperada: number = rnds.length / cantIntervalos;
-    let tabla : string[][] = [];
+    this.estadisticoAcum = 0;
+    this.tabla = [];
+    this.v = cantIntervalos-1;
     for (let i: number = 0; i < cantIntervalos; i++) {
       let limSuperior: number = limInferior + anchoIntervalo;
-      tabla.push([
+      let frecObservada = this.contarEnIntervalo(rnds, limInferior, limSuperior);
+      let estadistico : number = (Math.pow((frecObservada-frecEsperada),2)) / frecEsperada;
+      this.estadisticoAcum += estadistico;
+      this.tabla.push([
         limInferior + '-' + limSuperior,
-        //contarEnIntervalo(rnds, limInf, limSup)
+        frecObservada.toString(),
+        frecEsperada.toString(),
+        estadistico.toString(),
+        this.estadisticoAcum.toString(),
       ]);
       limInferior = limSuperior;
     }
-    return 0;
+    return this.estadisticoAcum;
+  }
+
+  public contarEnIntervalo(rnds : number[], limInferior: number, limSuperior: number ): number{
+    let cont : number = 0;
+    for (let i: number = 0; i < rnds.length; i++){
+      if(rnds[i] >= limInferior && rnds[i] < limSuperior)
+        cont++;
+    }
+    return cont
+  }
+
+  public validarHipotesis(): boolean{
+    // si el estadistico calculado es mayor al tabulado se rechaza la hipotesis nula 
+    if(this.estadisticoAcum > this.tablaChiCuadrado[this.v-1])
+      return false
   }
 }
